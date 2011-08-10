e54740b1580923ab085e02f40a541da7e3d1c24d
angel@SW.ad <angel.lopez@southworks.net>
2011-08-10 15:11:23 -0700

    TicTacToe ViewModel and Razor View, WIP

---
+++ main/code/SocialGames.TicTacToe/Scripts/game/TicTacToeViewModel.js
@@ -0,0 +1,14 @@
+﻿
+TicTacToeStage = { Create: 0, Join: 1, Wait: 2, Play: 3, Leave: 4 };
+
+function TicTacToeViewModel() {
+    this.stage = ko.observable(TicTacToeStage.Create);
+    this.playerName = ko.observable("user");
+    this.isOwner = ko.observable(false);
+    this.players = ko.observableArray();
+    this.gameQueueId = ko.observable(null);
+    this.gameId = ko.observable(null);
+    this.noPlayers = ko.observable(0);
+    this.inviteURL = ko.observable(null);
+}
+

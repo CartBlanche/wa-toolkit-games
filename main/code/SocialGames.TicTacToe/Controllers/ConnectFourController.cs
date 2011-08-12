c7a0db9fe90c108dd181602e0a14c3d7ac4129a9
mrodriguez <marcelo.rodriguez@southworks.net>
2011-08-12 16:12:19 -0700

    Merged conflict

---
+++ main/code/SocialGames.TicTacToe/Controllers/ConnectFourController.cs
@@ -0,0 +1,34 @@
+﻿namespace Microsoft.Samples.SocialGames.TicTacToe.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class ConnectFourController : Controller
+    {
+        public ActionResult Index()
+        {
+            this.SetConfigurationData();
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            this.SetConfigurationData();
+            return View();
+        }
+
+        public ActionResult OldBrowser()
+        {
+            return View();
+        }
+
+        private void SetConfigurationData()
+        {
+            this.ViewBag.BlobUrl = System.Configuration.ConfigurationManager.AppSettings["BlobUrl"];
+            this.ViewBag.ApiUrl = System.Configuration.ConfigurationManager.AppSettings["ApiUrl"];
+        }
+    }
+}

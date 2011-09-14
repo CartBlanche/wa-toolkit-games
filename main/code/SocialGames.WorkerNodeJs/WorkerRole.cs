e0ce6238468a31f106d7fca37875f6b1fe6618f9
angel@SW.ad <angel.lopez@southworks.net>
2011-09-14 13:01:59 -0700

    NodeJs Worker role improvement SocialGames

--- main/code/SocialGames.WorkerNodeJs/WorkerRole.cs
+++ main/code/SocialGames.WorkerNodeJs/WorkerRole.cs
@@ -1,28 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Net;
-using System.Threading;
-using Microsoft.WindowsAzure;
-using Microsoft.WindowsAzure.Diagnostics;
-using Microsoft.WindowsAzure.ServiceRuntime;
-using Microsoft.WindowsAzure.StorageClient;
-using System.IO;
-
-namespace WorkerRole1
+﻿namespace Microsoft.Samples.SocialGames.WorkerNodeJs
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Net;
+    using System.Threading;
+    using Microsoft.WindowsAzure;
+    using Microsoft.WindowsAzure.Diagnostics;
+    using Microsoft.WindowsAzure.ServiceRuntime;
+    using Microsoft.WindowsAzure.StorageClient;
+    using System.IO;
+
     public class WorkerRole : RoleEntryPoint
     {
         Process proc;
 
         public override void Run()
         {
-            proc.WaitForExit();
+            while (!proc.WaitForExit(10000))
+            {
+                if (!NodeIsOk())
+                    break; 
+            }
         }
 
         public override bool OnStart()
         {
+            Trace.TraceInformation("Microsoft.Samples.SocialGames.WorkerNodeJs.OnStart");
+            this.LaunchNode();
+            return base.OnStart();
+        }
+
+        private bool NodeIsOk()
+        {
+            Trace.TraceInformation("Testing Node.Js server");
+            return true;
+        }
+
+        private Process LaunchNode()
+        {
+            Trace.TraceInformation("Launching Node.Js server");
             proc = new Process()
             {
                 StartInfo = new ProcessStartInfo(
@@ -36,7 +54,7 @@ namespace WorkerRole1
             };
             proc.Start();
 
-            return base.OnStart();
+            return proc;
         }
     }
 }

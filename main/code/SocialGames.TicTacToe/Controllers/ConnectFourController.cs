e11c599e0d1f1fc2d80398c086bc728cd67c7aad
angel@SW.ad <angel.lopez@southworks.net>
2011-09-23 12:33:57 -0700

    Connect Four using NodeJs

--- main/code/SocialGames.TicTacToe/Controllers/ConnectFourController.cs
+++ main/code/SocialGames.TicTacToe/Controllers/ConnectFourController.cs
@@ -20,6 +20,12 @@
             return View();
         }
 
+        public ActionResult NodeJsEx()
+        {
+            this.SetConfigurationData();
+            return View();
+        }
+
         public ActionResult Login()
         {
             this.SetConfigurationData();
@@ -35,6 +41,7 @@
         {
             this.ViewBag.BlobUrl = System.Configuration.ConfigurationManager.AppSettings["BlobUrl"];
             this.ViewBag.ApiUrl = System.Configuration.ConfigurationManager.AppSettings["ApiUrl"];
+            this.ViewBag.NodeJsUrl = System.Configuration.ConfigurationManager.AppSettings["NodeJsUrl"];
         }
     }
 }

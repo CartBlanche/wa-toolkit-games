28f1a1621da695827e57434473ae104ced709901
mrodriguez <marcelo.rodriguez@southworks.net>
2011-09-27 07:23:31 -0700

    Fixed CA warning

--- main/code/SocialGames.Core/Comon/JobEngine/Conditions/Schedule.cs
+++ main/code/SocialGames.Core/Comon/JobEngine/Conditions/Schedule.cs
@@ -7,22 +7,29 @@
     {
         public static Func<bool> Every(TimeSpan interval)
         {
-            return new ScheduleCondition(interval).TickFunc;
+            using (var scheduleCondition = new ScheduleCondition(interval))
+            {
+                return scheduleCondition.TickFunc;
+            }            
         }
 
         public static Func<bool> Every(int milliseconds)
         {
-            return new ScheduleCondition(TimeSpan.FromMilliseconds(milliseconds)).TickFunc;
+            using (var scheduleCondition = new ScheduleCondition(TimeSpan.FromMilliseconds(milliseconds)))
+            {
+                return scheduleCondition.TickFunc;
+            }            
         }
 
-        private class ScheduleCondition
+        private class ScheduleCondition : IDisposable
         {
             private TimeSpan interval;
             private System.Timers.Timer timer;
-            private AutoResetEvent signal = new AutoResetEvent(false);
+            private AutoResetEvent signal;
 
             public ScheduleCondition(TimeSpan interval)
             {
+                this.signal = new AutoResetEvent(false);
                 this.interval = interval;
                 this.timer = new System.Timers.Timer(this.interval.TotalMilliseconds);
                 this.timer.Elapsed += (sender, arg) => { signal.Set(); };
@@ -39,6 +46,12 @@
                         return true;
                     };
                 }
+            }
+
+            public void Dispose()
+            {
+                this.signal.Dispose();
+                this.timer.Dispose();
             }
         }
     } 

8532f1248351ae6ef6265c3d55c5b413df51b355
mrodriguez <marcelo.rodriguez@southworks.net>
2011-09-28 09:26:57 -0700

    Fixed broken game service Test Removed GameServiceOldTest view

--- main/code/SocialGames.Web/Controllers/TestController.cs
+++ main/code/SocialGames.Web/Controllers/TestController.cs
@@ -1,47 +1,28 @@
 ﻿namespace Microsoft.Samples.SocialGames.Controllers
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Web;
     using System.Web.Mvc;
+    using Microsoft.Samples.SocialGames.Web.Controllers;
 
-    public class TestController : Controller
+    public class TestController : BaseController
     {
         public ActionResult Index()
         {
-            this.SetConfigurationData();
             return View();
         }
 
         public ActionResult ServerInterfaceTest()
         {
-            this.SetConfigurationData();
             return View();
         }
 
         public ActionResult GameServiceTest()
         {
-            this.SetConfigurationData();
-            return View();
-        }
-
-        public ActionResult GameServiceOldTest()
-        {
-            this.SetConfigurationData();
             return View();
         }
 
         public ActionResult UserServiceTest()
         {
-            this.SetConfigurationData();
             return View();
-        }
-
-        private void SetConfigurationData()
-        {
-            this.ViewBag.BlobUrl = System.Configuration.ConfigurationManager.AppSettings["BlobUrl"];
-            this.ViewBag.ApiUrl = System.Configuration.ConfigurationManager.AppSettings["ApiUrl"];
         }
     }
 }

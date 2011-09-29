bfe9ca4b48187d445044c4a0c1a5cd2bccba4c6e
mrodriguez <marcelo.rodriguez@southworks.net>
2011-09-29 09:33:22 -0700

    Updated dependency checker

--- main/setup/scripts/tasks/SetupDB.cmd
+++ main/setup/scripts/tasks/SetupDB.cmd
@@ -9,13 +9,23 @@ IF EXIST %WINDIR%\SysWow64 (
 	SET powerShellDir=%WINDIR%\system32\windowspowershell\v1.0
 )
 
+ECHO ------------------------------------------------------------------
+ECHO  After completing this setup please follow the instructions 
+ECHO  in the Readme document to run the sample.
+ECHO.
+ECHO  The setup will now create a database for the sample.
+ECHO ------------------------------------------------------------------
+PAUSE
+ECHO.
+ECHO.
+
 %powerShellDir%\powershell.exe -NonInteractive -Command "Set-ExecutionPolicy unrestricted"
 %powerShellDir%\powershell.exe -NonInteractive -command "%~dp0SetupDB"
 
 if errorlevel 1 (
-  echo -------------------------------------------------------------------------------
-  echo   Database creation failed. Please check error messages above.
-  echo -------------------------------------------------------------------------------
+   echo -------------------------------------------------------------------------------
+   echo   Database creation failed. Please check error messages above.
+   echo -------------------------------------------------------------------------------
 )
 
 PAUSE

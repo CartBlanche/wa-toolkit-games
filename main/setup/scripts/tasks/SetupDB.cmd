06f2cec909e3e5c5c8d209432f089260a87cb019
Alejandro Iglesias <alejandro.iglesias@southworks.net>
2011-10-27 17:21:19 -0300

    # install prowershell in 32 bits

--- main/setup/scripts/tasks/SetupDB.cmd
+++ main/setup/scripts/tasks/SetupDB.cmd
@@ -3,11 +3,7 @@ setlocal
 %~d0
 cd "%~dp0"
 
-IF EXIST %WINDIR%\SysWow64 (
-	SET powerShellDir=%WINDIR%\SysWow64\windowspowershell\v1.0
-) ELSE (
-	SET powerShellDir=%WINDIR%\system32\windowspowershell\v1.0
-)
+SET powerShellDir=%WINDIR%\system32\windowspowershell\v1.0
 
 %powerShellDir%\powershell.exe -NonInteractive -Command "Set-ExecutionPolicy unrestricted"
 %powerShellDir%\powershell.exe -NonInteractive -command "%~dp0SetupDB ..\database ..\..\.. ..\..\..\code\SocialGames.Cloud ..\..\..\code\SocialGames.Web"

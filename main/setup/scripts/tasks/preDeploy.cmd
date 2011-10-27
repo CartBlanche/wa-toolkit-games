9576ef66b765085d48e88a3f1c91d0e98c0744cb
Alejandro Iglesias <alejandro.iglesias@southworks.net>
2011-10-27 16:12:11 -0300

    # bug fix

--- main/setup/scripts/tasks/preDeploy.cmd
+++ main/setup/scripts/tasks/preDeploy.cmd
@@ -7,5 +7,3 @@ echo.
 echo ========= Running Pre-Deployment Script... =========
 powershell.exe -ExecutionPolicy Unrestricted -NonInteractive -command "%~dp0preDeployWAz ..\..\..\code\SocialGames.Web"
 echo =========   Pre-Deployment Script done!    =========
-
-@pause

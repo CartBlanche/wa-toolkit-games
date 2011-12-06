4ff688bea08ea923e3e90e2a2e8d15d3064cfff7
Nathan Totten <ntotten@microsoft.com>
2011-12-05 18:37:25 -0800

    Moved IInitializer to common folder.

---
+++ code/SocialGames.Core/Common/IInitializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.Samples.SocialGames.Common
+{
+    public interface IInitializer
+    {
+
+        void Initialize();
+
+    }
+}
